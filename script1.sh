9f47ea5d8028d9f31fd4f4d26d1fb1dc711da06b
jaydeep-22 <jayrajepandule2210@gmail.com>
2024-09-02 17:10:11 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,6 +1,5 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Git/Github"
-echo "we are learning gitup"
-echo "we are learning aws"
-echo " Hiii i am Devops engg"
+echo "we are learning AWS"
+
